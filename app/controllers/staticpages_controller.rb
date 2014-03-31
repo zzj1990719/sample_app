215bd7bba56873fdc733112446f2a6efd36ac26b
zzj1990719 <kevinnaspol@gmail.com>
2014-03-31 17:25:31 +0800

    Update commit

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,15 @@
+class StaticpagesController < ApplicationController
+  def home
+    @micropost = current_user.microposts.build if signed_in?
+    @feed_items = current_user.feed.paginate(page: params[:page]) if signed_in?
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '6dbea2e0fbce531ae77ac69bf528ca8f5228e001a5d89164bbbd5a8802a819a44eb1506e498ae6013164782bf6691eca9cc75fcae9b86d365aae42aa06ff23ec'
